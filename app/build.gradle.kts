251ef633ad5aab76e71959eca81b7926d515ddd2
Anjishnu Nandi <anjicroma@gmail.com>
2025-12-23 21:09:19 +0530

    Stable Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "chromahub.rhythm.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 38293818
-        versionName = "3.8.293.818"
+        versionCode = 38293819
+        versionName = "3.8.293.819"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
